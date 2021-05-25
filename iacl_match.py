00737b1bf7378a0cf2353562bc4a55a7ad50775f
Devon Lee <dslee@colgate.edu>
2021-05-25 13:00:50 -0400

    working dict

--- iacl_match.py
+++ iacl_match.py
@@ -23,23 +23,23 @@ def intraconfig_refs(cfile):
         line = infile.readline()
         if is_regex_match('^interface [a-zA-Z0-9\-]+', line):
             iName = getName(line, 1)
-            #print(iName)
+            #print(iName) 
             references = []
             line = infile.readline()
+            found_ref = False
             while (line.strip() != "!"):
                 if (is_regex_match('ip access-group [a-zA-Z0-9\-]+ ', line)):
+                    found_ref = True
                     ACLName = getName(line, 2)
                     references.append(ACLName)
                 line = infile.readline()
             #print(references)
-            IToACL[iName] = references  
-    print(IToACL)           
+            if found_ref:
+                IToACL[iName] = references  
+    print(IToACL)
+    with open('data.txt', 'w') as outfile:
+        json.dump(IToACL, outfile)           
     return IToACL
 
 
 intraconfig_refs("/shared/configs/northwestern/core1.conf")
-
-
-
-
-
