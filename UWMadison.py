9a616f92035d77bfb98e15ea65700d4bf88ccadd
Devon Lee <dslee@colgate.edu>
2021-06-03 16:55:02 -0400

    desc dict + vlan names for UWM

--- UWMadison.py
+++ UWMadison.py
@@ -18,11 +18,12 @@ def getName(line, tokenNum):
     return iName  
 
 #returns a dictionary with {interface name: [list of words in description]} 
-def get_descriptions(file):
+def get_descriptions(file, outf):
     infile = open(file, "r")
-    outf = "UWM testing"
     line = infile.readline()
     desc_dict = {}
+    vlan_names = []
+
     while line:
         if is_regex_match('^interface [a-zA-Z0-9\-]+', line):
             iName = getName(line, 1)
@@ -31,13 +32,25 @@ def get_descriptions(file):
                     temp = line.split()
                     desc_dict[iName] = temp[1:]
                 line = infile.readline()
-        line = infile.readline()
-    
+                
+        elif (is_regex_match('^vlan', line)):
+            temp = line.split()
+            if (len(temp) == 2):
+                line = infile.readline()
+                name = line.split()
+                if name[0] == "name":
+                    vlan_names.append(name[1])
+        line = infile.readline()    
 
     with open(outf, 'w') as outfile:
         outfile.write("desc_dict\n")
         json.dump(desc_dict, outfile, indent = 4)
 
-    return desc_dict
+        outfile.write("\n\n-----------------------------------\n")
 
-get_descriptions("/shared/configs/uwmadison/2014-10-core/configs/r-432nm-b3a-1-core.cfg")
+        outfile.write("vlan_names\n")
+        outfile.write(str(vlan_names))
+
+    return desc_dict, vlan_names
+
+get_descriptions("/shared/configs/uwmadison/2014-10-core/configs/r-432nm-b3a-2-core.cfg", "UWM testing")
