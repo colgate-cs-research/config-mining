05cc11846d2d5ffdd40f6cbe4bbf3d8b5b957dfe
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2021-06-07 12:46:46 -0400

    Update parsing script to make a manual pass over configs to extract VLAN names

--- parsing.py
+++ parsing.py
@@ -10,6 +10,7 @@ from pybatfish.datamodel.answer import *
 from pybatfish.datamodel.flow import *
 from pybatfish.question import *
 from pybatfish.question import bfq
+import re
 
 def main():
      #parsing command-line arguments
@@ -28,21 +29,32 @@ def main():
     bf_init_snapshot(snapshot_path, name=os.path.basename(snapshot_path), overwrite=True)
     load_questions()
 
+    configs_path = os.path.join(snapshot_path, "configs")
     json_path = os.path.join(snapshot_path, "configs_json")
     os.makedirs(json_path, exist_ok=True)
+
+    # Create dictionary mapping nodes to files
+    files = {}
+    data = bfq.fileParseStatus().answer().frame()
+    for i,row in data.iterrows():
+        if (len(row["Nodes"]) == 1):
+            files[row["Nodes"][0]] = row["File_Name"]
 
     # Get list of devices
     nodes = bfq.nodeProperties().answer().frame()
     for node in nodes["Node"]:
-        parts = extract_node(node)
+        with open(os.path.join(snapshot_path, files[node]), "r") as conf_file:
+            raw_lines = conf_file.readlines()
+        parts = extract_node(node, raw_lines)
         with open(os.path.join(json_path, node + ".json"), 'w') as json_file:
-            json.dump(parts, json_file, indent=4)
+            json.dump(parts, json_file, indent=4, sort_keys=True)
 
-def extract_node(node):
+def extract_node(node, raw_lines):
     parts = {
         "name" : node,
         "interfaces" : extract_interfaces(node),
-        "acls" : extract_acls(node)
+        "acls" : extract_acls(node),
+        "vlans" : extract_vlans(node, raw_lines)
     }
     return parts
 
@@ -107,6 +119,30 @@ def extract_acl_lines(data):
         lines.append(line)
 
     return lines
+
+#returns boolean indicating if argument regex pattern exists in argument string
+def is_regex_match(pattern, line):
+    p = re.compile(pattern)
+    iList = p.findall(line)
+    return (len(iList) > 0)
+
+def extract_vlans(node, raw_lines):
+    vlans = {}
+    i = 0
+    while i < len(raw_lines):
+        line = raw_lines[i]
+        if (is_regex_match('^vlan \d+$', line)):
+            temp = line.strip().split()
+            num = int(temp[1])
+            i += 1
+            line = raw_lines[i]
+            temp = line.strip().split()
+            if temp[0] == "name":
+                name = temp[1]
+                vlans[num] = { "num" : num, "name" : name }
+        i += 1
+
+    return vlans
 
 if __name__ == "__main__":
     main()
