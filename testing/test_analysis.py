d7f8aea5277f967d611c32bff6ee4e88a41ec542
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2021-06-11 13:09:12 -0400

    Reorganized and fixed testing suite

--- testing/test_analysis.py
+++ testing/test_analysis.py
@@ -10,11 +10,12 @@ testing_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(1, os.path.dirname(testing_dir))
 import analyze_refs
 
-def test_acls_iacl_match():
-    configs_dir = os.path.join(testing_dir, "parsing", "configs_json")
+configs_dir = os.path.join(testing_dir, "analysis", "configs_json")
+expected_dir = os.path.join(testing_dir, "analysis", "expected")
+
+def test_analyze_refs():
     config_file = os.path.join(configs_dir, "acls.json") 
-    expected_dir = os.path.join(testing_dir, "parsing", "expected", "iacl_match")
-    expected_file = os.path.join(expected_dir, "acls.json") 
+    expected_file = os.path.join(expected_dir, "analyze_refs", "acls.json") 
     out_dir = tempfile.mkdtemp()
     out_file = os.path.join(out_dir, "acls.json")
     analyze_refs.analyze_configuration(config_file, out_file)
