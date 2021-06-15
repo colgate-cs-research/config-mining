95ba426fc460da30bb68175a87f3ecdb2e3d7bce
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2021-06-15 09:00:55 -0400

    Expand test suite for analyze_keywords

--- testing/test_analyze_keywords.py
+++ testing/test_analyze_keywords.py
@@ -32,19 +32,28 @@ keywords = {
         ]
     },
     "acls": {
-        "green": [
+        "G": [
             "green"
         ],
-        "orange": [
+        "O": [
             "orange"
         ],
-        "red": [
+        "R": [
             "red"
         ],
-        "yellow": [
+        "Y": [
             "yellow"
         ]
     }
+}
+
+IfaceName2AppliedAclNames = {
+    "GigabitEthernet0/1": {},
+    "GigabitEthernet0/2": {"in": "R"},
+    "GigabitEthernet0/3": {"out": "O"},
+    "GigabitEthernet0/4": {"in": "Y", "out": "G"},
+    "GigabitEthernet0/5": {"in": "O", "out": "Y"},
+    "GigabitEthernet0/6": {"out": "Y"}
 }
 
 def test_get_common_keywords_once():
@@ -57,7 +66,7 @@ def test_get_common_keywords_thrice():
     expected = ["red", "yellow"]
     assert sorted(common) == sorted(expected)
 
-def test_keyword_interfaces():
+def test_keyword_stanza_interfaces():
     Keywords2IfaceNames = analyze_keywords.keyword_stanza(["red", "yellow", "blue"], keywords, "interfaces")
     expected = {
         "red" : ["GigabitEthernet0/2", "GigabitEthernet0/3", "GigabitEthernet0/5"],
@@ -68,13 +77,86 @@ def test_keyword_interfaces():
     for keyword in expected:
         assert sorted(Keywords2IfaceNames[keyword]) == sorted(expected[keyword])
 
-def test_keyword_acls():
+def test_keyword_stanza_acls():
     Keywords2AclNames = analyze_keywords.keyword_stanza(["red", "yellow", "blue"], keywords, "acls")
     expected = {
-        "red" : ["red"],
-        "yellow" : ["yellow"],
+        "red" : ["R"],
+        "yellow" : ["Y"],
         "blue" : []
     }
     assert sorted(Keywords2AclNames.keys()) == sorted(expected.keys())
     for keyword in expected:
         assert sorted(Keywords2AclNames[keyword]) == sorted(expected[keyword])
+
+def test_keyword_association():
+    Keywords2IfaceNames = {
+        "red" : ["GigabitEthernet0/2", "GigabitEthernet0/3", "GigabitEthernet0/5"],
+        "yellow" : ["GigabitEthernet0/3", "GigabitEthernet0/4", "GigabitEthernet0/5", "GigabitEthernet0/6"],
+        "blue" : ["GigabitEthernet0/1", "GigabitEthernet0/4"]
+    }
+    Keywords2AclNames = {
+        "red" : ["R"],
+        "yellow" : ["Y"],
+        "blue" : []
+    }
+    associations = analyze_keywords.keyword_association(IfaceName2AppliedAclNames, Keywords2IfaceNames, Keywords2AclNames)
+    expected = {
+        ("red", "R"): (1, 3),
+        ("yellow", "Y"): (3, 4)
+    }
+    assert associations == expected
+
+def test_keyword_ipaddress_range():
+    Keywords2IfaceNames = {
+        "red" : ["GigabitEthernet0/2", "GigabitEthernet0/3", "GigabitEthernet0/5"],
+        "yellow" : ["GigabitEthernet0/3", "GigabitEthernet0/4", "GigabitEthernet0/5", "GigabitEthernet0/6"],
+        "blue" : ["GigabitEthernet0/1", "GigabitEthernet0/4"]
+    } 
+    IfaceNames2Ips = {
+        "GigabitEthernet0/1": "10.0.1.1",
+        "GigabitEthernet0/2": "10.0.2.1",
+        "GigabitEthernet0/3": "10.0.3.1",
+        "GigabitEthernet0/4": "10.0.4.1",
+        "GigabitEthernet0/5": "10.0.5.1",
+        "GigabitEthernet0/6": "10.0.6.1",
+    }
+    expected_ranges = {
+        "red": ["10.0.2.1", "10.0.5.1"],
+        "yellow": ["10.0.3.1", "10.0.6.1"],
+        "blue": ["10.0.1.1", "10.0.4.1"]
+    }
+    expected_ips = {
+        "red": ["10.0.2.1", "10.0.3.1", "10.0.5.1"],
+        "yellow": ["10.0.3.1", "10.0.4.1", "10.0.5.1", "10.0.6.1"],
+        "blue": ["10.0.1.1", "10.0.4.1"]
+    }
+    Keywords2IpRanges, Keywords2Ips = analyze_keywords.keyword_ipaddress_range(Keywords2IfaceNames, IfaceNames2Ips)
+    assert Keywords2IpRanges == expected_ranges
+    assert Keywords2Ips == expected_ips
+
+def test_keyword_range_confidence():
+    IfaceNames2Ips = {
+        "GigabitEthernet0/1": "10.0.1.1",
+        "GigabitEthernet0/2": "10.0.2.1",
+        "GigabitEthernet0/3": "10.0.3.1",
+        "GigabitEthernet0/4": "10.0.4.1",
+        "GigabitEthernet0/5": "10.0.5.1",
+        "GigabitEthernet0/6": "10.0.6.1",
+    }
+    Keywords2IpRanges = {
+        "red": ["10.0.2.1", "10.0.5.1"],
+        "yellow": ["10.0.3.1", "10.0.6.1"],
+        "blue": ["10.0.1.1", "10.0.4.1"]
+    }
+    Keywords2Ips = {
+        "red": ["10.0.2.1", "10.0.3.1", "10.0.5.1"],
+        "yellow": ["10.0.3.1", "10.0.4.1", "10.0.5.1", "10.0.6.1"],
+        "blue": ["10.0.1.1", "10.0.4.1"]
+    }
+    associations = analyze_keywords.keyword_range_confidence(IfaceNames2Ips.values(), Keywords2IpRanges, Keywords2Ips)
+    expected = {
+        "red": [3, 4],
+        "yellow": [4, 4],
+        "blue": [2, 4]
+    }
+    assert associations == expected
