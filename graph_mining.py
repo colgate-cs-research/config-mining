989d132bb10069c11680bda17be65828fa66d337
Devon Lee <dslee@colgate.edu>
2021-06-18 13:26:41 -0400

    updated interface and vlan graph construction

--- graph_mining.py
+++ graph_mining.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import analyze
 import networkx as nx
+import ipaddress
+import re
 
 def load_file(file):
     # Load config
@@ -10,20 +12,29 @@ def load_file(file):
     return config
 
 
-#constructs a node in 
+#constructs nodes to fill in argument graph
 def fill_graph(edges, file, graph):
+    #for acl in file["acls"]:
+
+    regex = re.compile('Vlan\d+')
     for interface in file["interfaces"]:
-        graph.add_node(interface, type="interface")
-        for edge in edges:
-            node_type = edge[:-1]
-            if file["interfaces"][interface][edge] is not None:
-                graph.add_node(file["interfaces"][interface][edge], type=node_type)
-                graph.add_edge(interface, file["interfaces"][interface][edge])
+        #vlan
+        if regex.match(interface):
+            graph.add_node(interface, type="vlan")
+        else:    
+            graph.add_node(interface, type="interface")
+        
+        if file["interfaces"][interface]["address"] is not None:
+            address = file["interfaces"][interface]["address"]
+            network_obj = ipaddress.IPv4Interface(address)
+            graph.add_node(str(network_obj.network), type="subnet")
+            graph.add_edge(interface, str(network_obj.network))
+        if file["interfaces"][interface]["allowed_vlans"] is not None:
+            for vlan in file["interfaces"][interface]["allowed_vlans"]:
+                graph.add_edge(interface, "Vlan" + str(vlan))
     return 
 
-    #file[access_name] == file["interfaces"]
-    #file[vlans == file["vlans"]] ---> file[interfaces][vlans]
-
+''''
 def make_edges(group, edges, graph, file):
     for stanza in list(graph):
         if file[group][stanza][edges] is not None:
@@ -33,16 +44,14 @@ def make_edges(group, edges, graph, file):
                 #add more attributes to the nodes like the name alonged with it
                 #set node attributes
     return graph
-
+'''
 
 def main():
     config = load_file("/shared/configs/uwmadison/2014-10-core/configs_json/r-432nm-b3a-1-core.json")
     graph = nx.Graph() 
-    edges = ["access_vlan",  "address", "description"]
+    edges = ["address", "description", "allowed_vlans"]
     fill_graph(edges, config, graph)
-    #print(graph.nodes())
-    print(graph.edges())
-
+    #print(graph.edges())
     #make_edges("interfaces", "allowed_vlans", graph, config)
 
 if __name__ == "__main__":
