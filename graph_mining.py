24d9021ed8c54b43f608f9813d673912ff3c71d6
Emily Yu <eyu@colgate.edu>
2021-06-18 15:16:22 -0400

    Add to graph interfaces to keywords

--- graph_mining.py
+++ graph_mining.py
@@ -4,6 +4,7 @@ import analyze
 import networkx as nx
 import ipaddress
 import re
+from extract_keywords import get_keywords, add_keywords, analyze_configuration
 
 def load_file(file):
     # Load config
@@ -29,10 +30,20 @@ def fill_graph(edges, file, graph):
             network_obj = ipaddress.IPv4Interface(address)
             graph.add_node(str(network_obj.network), type="subnet")
             graph.add_edge(interface, str(network_obj.network))
+
         if file["interfaces"][interface]["allowed_vlans"] is not None:
             for vlan in file["interfaces"][interface]["allowed_vlans"]:
                 graph.add_edge(interface, "Vlan" + str(vlan))
     return 
+
+#Adding individual keywords to each interface
+def add_keywords(file, graph):
+    iface2keywords,_ = analyze_configuration(file, "output/testing.out")
+    for interface, keywords in iface2keywords.items():
+        graph.add_node(interface, type="interface")
+        for word in keywords:
+            graph.add_node(str(word), type="keyword")
+            graph.add_edge(interface, str(word))
 
 ''''
 def make_edges(group, edges, graph, file):
@@ -50,9 +61,14 @@ def main():
     config = load_file("/shared/configs/uwmadison/2014-10-core/configs_json/r-432nm-b3a-1-core.json")
     graph = nx.Graph() 
     edges = ["address", "description", "allowed_vlans"]
-    fill_graph(edges, config, graph)
+    #fill_graph(edges, config, graph)
     #print(graph.edges())
     #make_edges("interfaces", "allowed_vlans", graph, config)
+
+    add_keywords("/shared/configs/uwmadison/2014-10-core/configs_json/r-432nm-b3a-1-core.json", graph)
+    
+    #print(graph.edges())
+    #print(graph.nodes())
 
 if __name__ == "__main__":
     main()
