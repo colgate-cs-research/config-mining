30b3644161830dd4cfef49d1797505393b6785c7
Emily Yu <eyu@colgate.edu>
2021-06-24 14:39:34 -0400

    Co-authored-by: dlee-cs <dlee-cs@users.noreply.github.com> Co-authored-by: Aaron Gember-Jacobson <agember@users.noreply.github.com>

--- analyze_ospf.py
+++ analyze_ospf.py
@@ -2,6 +2,7 @@ import argparse
 import json
 from analyze_keywords import load_keywords, count_keywords, get_common_keywords, interface_ip_dictionary
 import analyze_refs
+import analyze
 
 
 def ospf_interfaces(config):
@@ -17,28 +18,28 @@ def ospf_interfaces(config):
 #Association rule: If the interface has a specific keyword, does that interface also run ospf
 #Returns a dictionary of interfaces as the key and a list of two numbers to determine the confidence
 #The first number is if the interface has the keyword and runs ospf and the second number is if the interface has the keyword
-def keywords_to_ospf(file, keyword):
-    config = load_keywords(file)
+def keywords_to_ospf(config_file, keyword_file):
+    keywords = load_keywords(keyword_file)
     interface_keyword_dict = {}
-    count_keywords(config, "interfaces", interface_keyword_dict)
+    count_keywords(keywords, "interfaces", interface_keyword_dict)
     common_keywords = get_common_keywords(interface_keyword_dict, threshold=10)
     keywords_to_ospf_dict = {}
+    
+    with open(config_file, 'r') as cf:
+        config = json.load(cf)
     ospf_interface = ospf_interfaces(config)
 
-    for iface_name in config["interfaces"]:
+    for word in common_keywords:
         iface_keyword = 0
         iface_keyword_ospf = 0
-        result = []
-        iface_words = config["interfaces"][iface_name]
-        for word in common_keywords:
+        for iface_name in keywords["interfaces"]:
+            iface_words = keywords["interfaces"][iface_name]
             if word in iface_words:
                 iface_keyword += 1
                 if iface_name in ospf_interface:
                     iface_keyword_ospf += 1
 
-        result.append(iface_keyword_ospf) 
-        result.append(iface_keyword)
-        keywords_to_ospf_dict[iface_name] = result
+        keywords_to_ospf_dict[word] = [iface_keyword_ospf, iface_keyword]
 
     return keywords_to_ospf_dict
 
@@ -50,22 +51,38 @@ def make_ospf_interface_dictionary(config):
 
     return ospf_interface_dictionary
 
-
+'''
 def address_in_range_ospf(file):
     iface_ipaddress_dictionary = interface_ip_dictionary(file)
     ospf_dictionary = make_ospf_interface_dictionary(file)
 
     range_dictionary,_ = keyword_ipaddress_range(ospf_interface_dict, interface_IPaddress_dict)
     print(range_dictionary)
+'''
+def analyze_configuration(in_paths, out_path, extra=None):
+    config_file, keyword_file = in_paths
+    #config = load_keywords(file)
+    dictionary = keywords_to_ospf(config_file, keyword_file)
+    rules = []
+    for keyword, values in dictionary.items():
+        rule = analyze.create_rule("iface has keyword "+keyword+" => iface runs OSPF", values[0], values[1])
+        rules.append(rule)
+
+    analyze.write_to_outfile(out_path, rules)
+    #ip_dictionary = interface_ip_dictionary(file)
+    #ospf_dictionary = make_ospf_interface_dictionary(config)
 
 def main():
-    file = "/shared/configs/northwestern/configs_json/core1.json"
-    config = load_keywords(file)
-    dictionary = keywords_to_ospf("/shared/configs/northwestern/configs_json/core1.json", "network")
-    ip_dictionary = interface_ip_dictionary(file)
-    ospf_dictionary = make_ospf_interface_dictionary(config)
-    #print(ospf_dictionary)
-    #address_in_range_ospf(config)
+    #parsing command-line arguments
+    parser = argparse.ArgumentParser(description='Analyze confidence for OSPF')
+    parser.add_argument('config_path', help='Path for a file (or directory) containing a JSON representation of configuration(s)')
+    parser.add_argument('keyword_path', help='Path for a file (or directory) containing a JSON representation of keyword(s)')
+    parser.add_argument('out_path', help='Name of file (or directory) to write JSON file(s) containing vlan confidence values')
+
+    arguments = parser.parse_args()
+    analyze.process_configs(analyze_configuration, [arguments.config_path, arguments.keyword_path], arguments.out_path)
+    
+
 
 
 if __name__ == "__main__":
