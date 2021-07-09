a3e3e6df371bf3312a21a492a1c80dcbf1da50fa
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2021-07-09 11:18:04 -0400

    Add command-line arguments to generate_graph

--- generate_graph.py
+++ generate_graph.py
@@ -1,14 +1,48 @@
+import analyze
 import argparse
-import json
-
-from networkx.algorithms.components.connected import connected_components, number_connected_components
-import networkx as nx
-#import pydot
 import ipaddress
+import json
+import networkx as nx
 import re
-from extract_keywords import analyze_configuration
 
-def load_file(file):
+def main():
+    #Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Generate a graph')
+    parser.add_argument('config_path',type=str, help='Path for a file (or directory) containing a JSON representation of configuration(s)')
+    parser.add_argument('keyword_path',type=str,help='Path for a file (or directory) containing a JSON representation of keywords from config(s)')
+    parser.add_argument('out_path',type=str,help='Path for a file (or directory) in which to store a JSON representation (and image) of the graph(s)')
+    parser.add_argument('-i', '--images', action='store_true', default=False)
+    arguments = parser.parse_args()
+
+    analyze.process_configs(analyze_configuration, [arguments.config_path, arguments.keyword_path], arguments.out_path, arguments.images)
+
+def analyze_configuration(in_paths, out_path=None, generate_image=False):
+    print("Current working files: %s" % (in_paths))
+    config_path, keyword_path = in_paths
+    config = load_config(config_path)
+    graph = make_graph(config)
+    add_keywords(keyword_path, graph)
+
+    # Save graph
+    if out_path is not None:
+        # Save graph
+        analyze.write_to_outfile(out_path, nx.readwrite.json_graph.node_link_data(graph))
+
+        # Save image
+        if generate_image:
+            pydot_graph = nx.drawing.nx_pydot.to_pydot(graph)
+            pydot_graph.write_png(out_path.replace(".json", ".png"))
+    
+    return graph
+
+def generate_graph(config_path, keyword_path):
+    #from networkx.algorithms.components.connected import connected_components, number_connected_components
+    #print("\nComponents in graph: " + str(number_connected_components(graph)))
+    #for component in connected_components(graph):
+    #print(component)
+    return analyze_configuration((config_path, keyword_path))
+
+def load_config(file):
     # Load config
     with open(file, "r") as infile:
         config = json.load(infile)
@@ -24,10 +58,8 @@ def make_graph(config):
     regex = re.compile(r'Vlan\d+')
     for interface in config["interfaces"]:
         if regex.match(interface):
-            print(interface, "vlan")
             graph.add_node(interface, type="vlan")
         else:    
-            print(interface, "interface")
             graph.add_node(interface, type="interface")
         
         if config["interfaces"][interface]["address"] is not None:
@@ -51,31 +83,16 @@ def make_graph(config):
 
 #Adding individual keywords to each interface
 def add_keywords(keyword_path, graph):
+    # Load keywords
     with open(keyword_path, 'r') as keyword_file:
         keyword_json = json.load(keyword_file)
+
+    # Add keyword nodes and edges
     for interface, keywords in keyword_json["interfaces"].items():
         #graph.add_node(interface, type="interface")
         for word in keywords:
             graph.add_node(str(word), type="keyword")
             graph.add_edge(interface, str(word))
 
-def generate_graph(config_path, keyword_path):
-    config = load_file(config_path)
-    #config = load_file("/shared/configs/uwmadison/2014-10-core/configs_json/r-432nm-b3a-1-core.json") 
-    graph = make_graph(config)
-    add_keywords(keyword_path, graph)
-    
-    #---------------------------------------------------
-   
-    #print("\nComponents in graph: " + str(number_connected_components(graph)))
-    #for component in connected_components(graph):
-    #print(component)
-    
-    #pydot_graph = nx.drawing.nx_pydot.to_pydot(graph)
-    #pydot_graph.write_png('generated_graph.png')
-    return graph
-
-'''
 if __name__ == "__main__":
     main()
-'''
