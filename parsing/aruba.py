fe1f68116c3a6339c3b7cd30cc6e0a7574c72df3
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2021-10-13 21:04:53 -0400

    Finished script for parsing Aruba configs

--- parsing/aruba.py
+++ parsing/aruba.py
@@ -25,17 +25,14 @@ def main():
         parts = extract_node(node, conf)
         with open(os.path.join(json_path, node + ".json"), 'w') as json_file:
             json.dump(parts, json_file, indent=4, sort_keys=True)
-        break
 
 def extract_node(node, conf):
     parts = {
         "name" : node,
         "interfaces" : extract_interfaces(conf),
-#        "acls" : extract_acls(conf),
-#        "vlans" : extract_vlans(conf),
+        "acls" : extract_acls(conf),
+        "vlans" : extract_vlans(conf),
     }
-#    extract_acl_remarks(parts, raw_lines)
-#    resolve_acls_aliases(parts["acls"])
     return parts
 
 def extract_interfaces(conf):
@@ -52,12 +49,63 @@ def extract_interfaces(conf):
             "address" : (port["ip4_address"] if "ip4_address" in port else None),
             "switchport" : (None if len(port) == 0 else ("trunk" if "vlan_trunks" in port else "access")),
             "access_vlan" : (port["vlan_tag"] if "vlan_tag" in port else None),
-            "allowed_vlans" : (sorted([int(v) for v in port["vlan_trunks"]]) if "vlan_trunks" in port else None)
+            "allowed_vlans" : (sorted([int(v) for v in port["vlan_trunks"]]) if "vlan_trunks" in port else None),
+            "in_acl" : None, # FIXME
+            "out_acl" : None # FIXME
         }
 
         interfaces[interface["name"]] = interface
 
     return interfaces
+
+def extract_acls(conf):
+    """Extract details for all ACLs on a node"""
+    acls = {}
+    for acl_conf in conf["ACL"].values():
+        lines, remarks = extract_acl_parts(acl_conf["cfg_aces"])
+        acl  = {
+            "name" : acl_conf["name"],
+            "lines" : lines,
+            "remarks" : remarks
+        }
+
+        acls[acl["name"]] = acl
+    
+    return acls
+
+def extract_acl_parts(acl_conf):
+    """Extract match criteria for all lines in an ACL"""
+    lines = []
+    remarks = []
+
+    for priority in sorted([int(prio) for prio in acl_conf.keys()]):
+        line_conf = acl_conf[str(priority)]
+        line = {
+            "action" : (line_conf["action"] if "action" in line_conf else None),
+            "srcIps" : (str(ipaddress.ip_network(line_conf["src_ip"])) if "src_ip" in line_conf else None),
+            "dstIps" : (str(ipaddress.ip_network(line_conf["dst_ip"])) if "dst_ip" in line_conf else None),
+        }
+
+        if line["action"] is not None:
+            lines.append(line)
+
+        if "comment" in line_conf:
+            remarks.append(line_conf["comment"])
+    
+    return lines, remarks
+
+def extract_vlans(conf):
+    """Extract all VLAN names from all VLANs on a node"""
+    vlans = {}
+    for vlan_conf in conf["VLAN"].values():
+        vlan = {
+            "num" : vlan_conf["id"],
+            "name" : vlan_conf["name"]
+        }
+        
+        vlans[vlan["num"]] = vlan
+
+    return vlans
 
 if __name__ == "__main__":
     main()
