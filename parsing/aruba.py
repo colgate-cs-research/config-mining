ba650d53d2b939ba0f358681eefe7d1f604ff9ca
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2022-03-10 08:38:30 -0500

    Tweaks to Aruba parsing script

--- parsing/aruba.py
+++ parsing/aruba.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
+import fileinput
 import ipaddress
 import json
 import os
+import sys
 
 def main():
     # Parse command-line arguments
@@ -21,11 +23,33 @@ def main():
     for filename in sorted(os.listdir(configs_path)):
         node = filename.replace(".colgate.edu.json", "")
         print(node)
-        with open(os.path.join(configs_path, filename), "r") as conf_file:
-            conf = json.load(conf_file)
+        infilepath = os.path.join(configs_path, filename)
+        try:
+            with open(infilepath, "r") as conf_file:
+                conf = json.load(conf_file)
+        except json.decoder.JSONDecodeError as ex:
+            fix_aruba_json(infilepath)
+            try:
+                with open(infilepath, "r") as conf_file:
+                    conf = json.load(conf_file)
+            except json.decoder.JSONDecodeError as ex:
+                print("Failed to parse {}: {}".format(filename, ex))
+                continue
         parts = extract_node(node, conf)
         with open(os.path.join(json_path, node + ".json"), 'w') as json_file:
             json.dump(parts, json_file, indent=4, sort_keys=True)
+
+def fix_aruba_json(filepath):
+    with fileinput.FileInput(filepath, inplace='True') as conf_file:
+        for line in conf_file:
+            if '"cfg_version"' in line and line.count('"') != 4:
+                pre, post = line.rstrip().split(':')
+                print('{}: "{}",'.format(pre, post[1:-1]))
+            elif '"name"' in line and line.count('"') != 4:
+                pre, post = line.rstrip(",\n").split(':')
+                print('{}: {}"{}'.format(pre, post[1:], ("," if line[-2] == "," else "")))
+            else:
+                print(line, end='')
 
 def extract_node(node, conf):
     parts = {
