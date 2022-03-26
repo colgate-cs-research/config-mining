44b77ad66f54bc5495c3f0795b4557fb4ebe93b2
Sara <salam@colgate.edu>
2022-03-26 16:41:51 -0400

    juniper_split returns the config as a dictionary

--- parsing/juniper_split.py
+++ parsing/juniper_split.py
@@ -3,6 +3,42 @@
 import argparse
 import os
 
+def get_dict(lst, d):
+    i = 0
+    while i < len(lst):
+        #print(i)
+        s = lst[i]
+        if "{" in s:
+            key = s.strip().split()[0]
+            start = i + 1
+            end = start
+            occ = 1
+            while (occ != 0):
+                if "{" in s:
+                    occ += 1
+                if "}" in s:
+                    occ -= 1
+                end += 1
+                i += 1
+                if i< len(lst):
+                    s = lst[i]
+                else:
+                    break
+            #print("recursive call")
+            d2 = {}
+            lst2 = lst[start:end]
+            get_dict(lst2,d2)
+            d[key] = d2
+        #base case
+        else:
+            key = s.strip()
+            d[key] = None
+        i+= 1
+    return
+
+
+
+
 def main():
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Commandline arguments')
@@ -18,8 +54,13 @@ def main():
         # TODO: iterate over configuration file and find sections
 
         # Sample code for writing to file
-        section_name = "interfaces"
-        section_contents = "interfaces {\n    testing\n}"
+        '''section_name = "interfaces"
+        section_contents = "interfaces {\n    testing\n}"'''
+        section_name = "alba_dict"
+        lst = cfg_file.read().split("\n")
+        d = {}
+        get_dict(lst, d)
+        section_contents = str(d)
         with open(os.path.join(arguments.output_path, section_name+".cfg"), 'w') as out_file:
             out_file.write(section_contents)
 
