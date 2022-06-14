7c51ba6cc835db541ca7cba5d9ab10981cba7894
jchauhan <jchauhan@colgate.edu>
2022-06-13 20:55:27 -0400

    run_stucco now uses parallezied stucco.py

--- csl/run_stucco.py
+++ csl/run_stucco.py
@@ -4,9 +4,25 @@ import argparse
 import os
 import pandas
 import sys
-
+import datetime
+import time
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'libraries', 'contrast'))
-import stucco
+#import stucco_parallel1 as stucco
+import stucco_parallel2 as stucco
+#import stucco as stucco
+def update_time_measurement(file_name,depth,keyword,time_taken):
+    '''
+    Save time measurement to file
+    '''
+    now = datetime.datetime.now()
+    d4 = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    file = open("./csl_runtime_dataset.txt", "a")
+
+    date_entry = "date:"+str(d4)+" | "
+    L = date_entry+"input:"+file_name.split("/")[-2] + "/" +file_name.split("/")[-1] +" | " +"depth:"+str(depth)+" | "+"keyword:"+ keyword + " | "+"time_taken:"+ str(time_taken)+"\n"
+    file.writelines(L)
+    file.close()
 
 def main():
     # Parse command-line arguments
@@ -27,7 +43,15 @@ def main():
         data.drop("filename", inplace=True, axis=1)
 
     csl = stucco.ContrastSetLearner(data, group_feature=arguments.feature, max_real_bias=1)
+    
+    # time measuring
+    start_time = time.process_time()
     csl.learn(max_length=arguments.depth)
+    # stopping time measurement
+    execution_time = time.process_time()-start_time
+    # updating time measurement in the 
+    update_time_measurement(arguments.db_path,arguments.depth,arguments.feature,execution_time)
+    
     results = csl.score(min_lift=0) 
     print(results)
 
