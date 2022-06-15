c4943ff07ff6ae44719d1e2347069fb7bc6f0996
Sara <salam@colgate.edu>
2022-06-15 14:55:36 -0400

    closest_node.py done

--- parsing/closest_node.py
+++ parsing/closest_node.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import argparse
 import ipaddress
 import json
@@ -18,16 +19,20 @@ def distance(l1, l2):
     return d/len(l1)
 
 def get_closest_nodes(node_lst,vector_dict):
-    closest_nodes_dict = {}
+    closest_nodes_dict = {} 
     for i in range(len(node_lst)):
-        closest_nodes_dict[node_lst[i]] = []
+        closest_nodes_dict[node_lst[i]] = (10**9, None)
     for i in range(len(node_lst)):
         for j in range(i+1, len(node_lst)):
             d = distance(vector_dict[node_lst[i]], vector_dict[node_lst[j]])
-            if (d > 0.5):
-                closest_nodes_dict[node_lst[i]].append(node_lst[j])
-                closest_nodes_dict[node_lst[j]].append(node_lst[i])
-    return get_closest_nodes
+            if d < closest_nodes_dict[node_lst[i]][0]:
+                closest_nodes_dict[node_lst[i]] = (d, node_lst[j])
+            if d < closest_nodes_dict[node_lst[j]][0]:
+                closest_nodes_dict[node_lst[j]] = (d, node_lst[i])
+            #if (d < 0.001):
+            #    closest_nodes_dict[node_lst[i]].append(node_lst[j])
+            #    closest_nodes_dict[node_lst[j]].append(node_lst[i])
+    return closest_nodes_dict
 
 def main():
     start = time.time()
@@ -37,24 +42,31 @@ def main():
     arguments = parser.parse_args()
 
     #get dictionary where keys are interfaces and values are the vectors (list of numbers)
-    infile = open(arguments.vector_path, 'r')
+    f = open(arguments.vector_path, 'r')
+    line_dict = {}  # lines : counts
     vector_dict = {}
     node_lst = []
-    line = infile.readline().strip().split(",")
+    line = f.readline().strip().split(",")
     typ = line[0]
     lst_of_possible_neighbors = line[1:]
-    for line in infile:
+    for line in f:
         l = line.strip().split(",")
         name = l[0]
-        node_lst.append(name)
-        vector_dict[name] = l[1:]
-    infile.close()
+        vector = ",".join(l[1:])
+        if vector not in line_dict:
+            line_dict[vector] = []
+            node_lst.append(name)
+            vector_dict[name] = l[1:]
+        line_dict[vector].append(name)
+        #vector_dict[name] = l[1:]
+    f.close()
 
+    print(len(lst_of_possible_neighbors))
+    print(len(line_dict.keys()))
     closest_nodes_dict = get_closest_nodes(node_lst,vector_dict)
 
-    for key in get_closest_nodes:
-        print("Key: " + key)
-        print(get_closest_nodes[key])
+    for key in closest_nodes_dict:
+        print("Key: " + key + " Closest node: " + str(closest_nodes_dict[key]))
     
     end = time.time()
     print("Time taken: " + str(end - start))
