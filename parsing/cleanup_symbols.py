1397c9c5d6eaadd9d7614a4d6c3d68522b054d48
Aaron Gember-Jacobson <agemberjacobson@colgate.edu>
2022-06-24 13:51:47 -0400

    Move symbol table inversion to cleanup_symbols

--- parsing/cleanup_symbols.py
+++ parsing/cleanup_symbols.py
@@ -6,6 +6,7 @@ import networkx as nx
 import os
 import time
 from queue import Queue
+import logging
 
 
 # Function for aliasing
@@ -38,22 +39,47 @@ def similar_letters(s1, s2):
         return True
     return False
 
+def invert_table(symbol_table):
+    inverted_table = {}
+    for symbol_name, symbol_types in symbol_table.items():
+        for symbol_type in symbol_types:
+            if symbol_type not in inverted_table:
+                inverted_table[symbol_type] = set()
+            inverted_table[symbol_type].add(symbol_name)
+    return inverted_table
 
 def main():
     start = time.time()
+    
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Cleanup inferred symbols")
+    #parser.add_argument('configs_path', help='Path to directory of configurations')
+    parser.add_argument('symbols_dir', help='Path to directory containing symbol files')
+    parser.add_argument('output_dir', help='Path to directory in which to store output')
+    parser.add_argument('-v', '--verbose', action='count', help="Display verbose output", default=0)
+    arguments = parser.parse_args()
 
-    # parser = argparse.ArgumentParser(description='Get the list of most similar neighbors')
-    # parser.add_argument('inverted_path', help='Path for a file (or directory) containing a JSON representation of configuration(s)')
-    # parser.add_argument('out_path', help='Name of file (or directory) to write JSON file(s) containing keywords')
-    # output path for inverted_cleaned.json files
-    # arguments = parser.parse_args()
+    # module-wide logging
+    if (arguments.verbose == 0):
+        logging.basicConfig(level=logging.WARNING)
+    elif (arguments.verbose == 1):
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger(__name__)
 
-   
+    # Load symbols results
+    with open(os.path.join(arguments.symbols_dir, "symbols.json"), 'r') as symbols_file:
+        symbol_table = json.load(symbols_file)
+
+    inverted_table = invert_table(symbol_table)
+
+    # Save results
+    with open(os.path.join(arguments.output_dir, "inverted.json"), 'w') as inverted_file:
+        json.dump(inverted_table, inverted_file, indent=4, sort_keys=True)
+
     end = time.time()
     print("Time taken: " + str(end - start))
-
-
-
 
 if __name__ == "__main__":
     main()
